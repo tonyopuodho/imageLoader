81bf06b37ab5d4a13b2dfb1ac9bcbc02b1d46c62
tonyopuodho <tonyopuodho@gmail.com>
2026-03-23 18:23:55 +0300

    displaying images

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const productContainer = document.querySelector(".product-container")
+const loadBtn = document.querySelector(".loadBtn")
 
-async function fetchProducts(){
+async function fetchProducts(getCurrent){
     try {
-        const apiResponse = await fetch('https://dummyjson.com/products')
+        const apiResponse = await fetch(`https://dummyjson.com/products?limit=10&skip=${getCurrent === 0 ? 0 : getCurrent * 10}`)
         const apiResult = await apiResponse.json()
         if (apiResult && apiResult.products) displayProducts(apiResult.products)        
     } catch (error) {
@@ -10,5 +11,36 @@ async function fetchProducts(){
     }
 }
 
+
+function displayProducts(products){
+    products.forEach((items) => {
+
+        const itemWrapper = document.createElement("div")
+        const productTitle = document.createElement("p")
+        const productImage = document.createElement("img")
+        const productDescription = document.createElement("p")
+        const productPrice = document.createElement("p")
+
+        productTitle.textContent = items.title
+        productImage.src = items.thumbnail
+        productDescription.textContent = items.description
+        productPrice.textContent = items.price
+
+        productTitle.classList.add("product-title")
+        productImage.classList.add("product-image")
+        productDescription.classList.add("product-description")
+        productPrice.classList.add("product-price")
+        itemWrapper.classList.add("item")
+
+        itemWrapper.appendChild(productImage)
+        itemWrapper.appendChild(productTitle)
+        itemWrapper.appendChild(productPrice)
+        itemWrapper.appendChild(productDescription)
+
+        productContainer.appendChild(itemWrapper)
+
+    })
+   
+}
 
 fetchProducts()
